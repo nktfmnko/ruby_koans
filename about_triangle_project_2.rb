ed3330e4d52508e5dd81121727e73bea65e6dd81
nktfmnko <115153474+nktfmnko@users.noreply.github.com>
2024-04-22 22:30:57 +0300

    Add files via upload

---
+++ about_triangle_project_2.rb
@@ -0,0 +1,28 @@
+require File.expand_path(File.dirname(__FILE__) + '/neo')
+
+# You need to write the triangle method in the file 'triangle.rb'
+
+def triangle(a, b, c)
+  if a == b && a == c
+    :equilateral
+  elsif (a == b) || (b == c) || (a == c)
+    :isosceles
+  else
+    :scalene
+  end
+end
+
+# Error class used in part 2.  No need to change this code.
+class TriangleError < StandardError
+end
+class AboutTriangleProject2 < Neo::Koan
+  # The first assignment did not talk about how to handle errors.
+  # Let's handle that part now.
+  def test_illegal_triangles_throw_exceptions
+    assert_raise(TriangleError) do triangle(0, 0, 0) end
+    assert_raise(TriangleError) do triangle(3, 4, -5) end
+    assert_raise(TriangleError) do triangle(1, 1, 3) end
+    assert_raise(TriangleError) do triangle(2, 4, 2) end
+    # HINT: for tips, see http://stackoverflow.com/questions/3834203/ruby-koan-151-raising-exceptions
+  end
+end
